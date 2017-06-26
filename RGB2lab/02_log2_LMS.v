60db94bf6a3f7364e9f941c3184d49c8fd9e17ff
DebbieChou <tfgscdebbie@gmail.com>
2017-06-26 02:28:56 -0500

    integ

--- RGB2lab/02_log2_LMS.v
+++ RGB2lab/02_log2_LMS.v
@@ -8,21 +8,21 @@ input i_rst;
 input [15:0] i_index; // 8+8 B
 output [15:0] o_log2_index; // 3+13 B
 
-reg [2:0] int; //整數部分 3bits
+reg [2:0] integ; //整數部分 3bits
 reg [12:0] fraction; //小數部分 13bits
 
-assign o_log2_index = {int, fraction};
+assign o_log2_index = {integ, fraction};
 
 always @(*) begin
-	if(i_rst) int = 0;
-	else if(i_index > 1 && i_index < 4) int = 1;
-	else if(i_index > 3 && i_index < 8) int = 2;
-	else if(i_index > 7 && i_index < 16) int = 3;
-	else if(i_index > 15 && i_index < 32) int = 4;
-	else if(i_index > 31 && i_index < 64) int = 5;
-	else if(i_index > 63 && i_index < 128) int = 6;
-	else if(i_index > 127 && i_index < 256) int = 7;
-	else int = 0;
+	if(i_rst) integ = 0;
+	else if(i_index > 1 && i_index < 4) integ = 1;
+	else if(i_index > 3 && i_index < 8) integ = 2;
+	else if(i_index > 7 && i_index < 16) integ = 3;
+	else if(i_index > 15 && i_index < 32) integ = 4;
+	else if(i_index > 31 && i_index < 64) integ = 5;
+	else if(i_index > 63 && i_index < 128) integ = 6;
+	else if(i_index > 127 && i_index < 256) integ = 7;
+	else integ = 0;
 end
 
 
